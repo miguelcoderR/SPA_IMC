d648030f1452a3348822fc3612b4318c5ba77a6b
Miguel Ortega Cantillo <miguelocantillo@gmail.com>
2025-06-16 15:59:42 -0500

    FEAT: JAVA

---
+++ src/incons/img/java/java.js
@@ -0,0 +1,9 @@
+let myWeihgt
+let myHeight
+let imc
+myWeight=prompt(message ="plase, write your weight")
+myHeight=prompt(message ="plase, write your height")
+
+imc= myWeight/(myHeight * myHeight)
+
+alert("YOUR IMC is "+ imc )
